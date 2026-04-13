ab0de64c8dd7727c8d1857e459d8922569881ed3
mcanteros-hash <mcanteros@unicartagena.edu.co>
2026-04-13 11:17:56 -0500

    Empeado.java

---
+++ src/modelos/Empeado.java
@@ -0,0 +1,15 @@
+package modelos;
+
+public abstract class Empleado {
+    protected String nombre;
+    protected double salarioBase;
+    protected int antiguedad;
+
+    public Empleado(String nombre, double salarioBase, int antiguedad) {
+        this.nombre = nombre;
+        this.salarioBase = salarioBase;
+        this.antiguedad = antiguedad;
+    }
+
+    public abstract double calcularSalarioBruto();
+}
